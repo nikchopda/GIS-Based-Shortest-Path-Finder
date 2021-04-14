99271ff56ef757bbdfc9017dcbe94d8c78971ee3
Nikunj Chopda <nikunjchopda1996@gmail.com>
2021-04-13 23:29:29 -0400

    Added JAVA file "LoginController.java"

---
+++ JAVA Files/LoginController.java
@@ -0,0 +1,75 @@
+
+
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+/**
+ * Servlet implementation class LoginController
+ */
+@WebServlet("/LoginController")
+public class LoginController extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public LoginController() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		String un=request.getParameter("username");
+		String pw=request.getParameter("password");
+		
+			
+		try {
+			 
+			
+			 HttpSession session=request.getSession();
+		      session.setAttribute("uname",un);
+		      session.setAttribute("pass",pw);	
+		 Class.forName("org.postgresql.Driver"); 
+         Connection con = DriverManager.getConnection("jdbc:postgresql://localhost:5432/postgres?user=postgres&password=admin");
+         con.setAutoCommit(false);
+        
+		PreparedStatement ps = con.prepareStatement("select username,password from register where username=? and password=?");
+		ps.setString(1, un);
+		ps.setString(2, pw);
+ 
+		ResultSet rs = ps.executeQuery();
+ 
+		while (rs.next()) {
+			response.sendRedirect("module.jsp");
+			return;
+		}
+		response.sendRedirect("error.html");
+		return;
+		} catch (ClassNotFoundException | SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+
+}
